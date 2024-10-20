e274f52eb9bf624943288b0d82f561e92390e018
YipiReb3lde <caosrebeld3@gmail.com>
2024-10-20 16:44:39 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-const mobileMenu = document.getElementById('mobile-menu');
-const navList = document.querySelector('.nav-list');
+document.addEventListener("DOMContentLoaded", () => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.querySelector('.nav-list');
 
-// Abre o cierra el menú al hacer clic en el botón
-mobileMenu.addEventListener('click', () => {
-    navList.classList.toggle('active');
+    // Abre o cierra el menú al hacer clic en el botón
+    mobileMenu.addEventListener('click', () => {
+        navList.classList.toggle('active');
+    });
 });
